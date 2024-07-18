bdfdcb505f3b82308f9f42af2c2518bf088a8a3f
Дарья <167187299+Kaishivee@users.noreply.github.com>
2024-07-18 12:33:38 +0500

    Очереди для обмена данными между потоками.py

--- Очереди для обмена данными между потоками.py
+++ Очереди для обмена данными между потоками.py
@@ -2,69 +2,76 @@ from threading import Thread
 import queue
 from time import sleep
 
-# number(int) - номер стола, is_busy(bool) - занят стол или нет.
-
 
 class Table:
     def __init__(self):
         self.number = 1
         self.is_busy = False
 
-# 1.Атрибуты queue - очередь посетителей (создаётся внутри init), tables список столов (поступает из вне).
-# 2.Метод customer_arrival(self) - моделирует приход посетителя(каждую секунду).
-# 3.Метод serve_customer(self, customer) - моделирует обслуживание посетителя.
-# Проверяет наличие свободных столов, в случае наличия стола - начинает обслуживание посетителя (запуск потока),
-# в противном случае - посетитель поступает в очередь. Время обслуживания 5 секунд.
+    def set_busy(self):
+        self.is_busy = True
+
+    def set_free(self):
+        self.is_busy = False
 
 
 class Cafe:
     def __init__(self, tables):
-        self.queue = []
+        self.queue = queue
         self.tables = tables
+        self.number = 1
 
     def customer_arrival(self):
-        customer = 0
-        while customer < 20:
-            customer += 1
+        customer_number = 1
+        while customer_number <= 20:
+            print(f'Посетитель номер {customer_number} прибыл')
+            customer = Customer(customer_number, self)
+            customer.start()
+            customer_number += 1
             sleep(1)
-            print(f'Посетитель номер {customer} прибыл')
 
-    def serve_customer(self, customer):
-        self.number = 1
-        while self.number <= 3:
-            self.number += 1
-            print(f'Посетитель номер {customer} сел за стол {self.number}')
-            sleep(5)
-            print(f'Посетитель номер {customer} покушал и ушёл')
-            self.number -= 1
-        if self.number == 3:
-            print(f'Посетитель номер {customer} ожидает свободный стол')
+    def serve_customer(self, customer_number):
+        for table in self.tables:
+            if not table.is_busy:
+                table.set_busy()
+                print(f'Посетитель номер {customer_number} сел за стол {self.number}')
+                self.number += 1
+                sleep(5)
+                print(f'Посетитель номер {customer_number} покушал и ушёл')
+                table.set_free()
+                self.check_queue()
+                return
+        else:
+            self.queue.put(customer_number)
+            print(f'Посетитель номер {customer_number} ожидает свободный стол')
+
+    def check_queue(self):
+        if not self.queue.empty():
+            customer_number = self.queue.get()
+            self.serve_customer(customer_number)
 
 
-# Customer - класс (поток) посетителя. Запускается, если есть свободные столы.
-# Так же должны выводиться текстовые сообщения соответствующие событиям:
-# Посетитель номер <номер посетителя> прибыл.
-# Посетитель номер <номер посетителя> сел за стол <номер стола>. (начало обслуживания)
-# Посетитель номер <номер посетителя> покушал и ушёл. (конец обслуживания)
-# Посетитель номер <номер посетителя> ожидает свободный стол. (помещение в очередь)
+class Customer(Thread):
+    def __init__(self, num, cafe):
+        super().__init__()
+        self.num = num
+        self.cafe = cafe
+
+    def run(self):
+        self.cafe.serve_customer(self.num)
 
 
-class Customer:
-    def __init__(self):
-        pass
-
+queue = queue.Queue()
 
 table1 = Table()
 table2 = Table()
 table3 = Table()
 tables = [table1, table2, table3]
 
-# Инициализируем кафе
 cafe = Cafe(tables)
 
-# Запускаем поток для прибытия посетителей
+
 customer_arrival_thread = Thread(target=cafe.customer_arrival)
 customer_arrival_thread.start()
 
-# Ожидаем завершения работы прибытия посетителей
 customer_arrival_thread.join()
